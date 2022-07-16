1ed13ddde27decfcc112452d18cc66ed2360a323
Dionis <martinsdionis@gmail.com>
2022-07-15 23:48:36 -0300

    nav host implementatin

--- app/src/main/java/com/dionis/auladokevyn/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/dionis/auladokevyn/presentation/activity/MainActivity.kt
@@ -2,14 +2,26 @@ package com.dionis.auladokevyn.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.dionis.auladokevyn.R
+import com.dionis.auladokevyn.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
+    private var navHostFragment : NavHostFragment? = null
+    private var navController : NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment?.navController
     }
 }
